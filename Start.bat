c4b365efa641f4a648de69a0ba56470f8cf5eb77
soulaymanaitahmed <soulaymanaitahmed39@gmail.com>
2026-04-21 18:25:52 +0100

    Update the base project

--- Start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start "" cmd /c "cd Back-end && npm start"
-start "" cmd /c "cd Front-End && npm start"
--- Stop.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Close the React app (served by serve)
-taskkill /F /IM serve.exe /T
-
-REM Close the Express app (run by npm start)
-taskkill /F /IM node.exe /T
