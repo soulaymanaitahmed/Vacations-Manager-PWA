e590363d6541d6bd7ee3c715ef7a5a16f95005d6
soulaymanaitahmed <soulaymanaitahmed39@gmail.com>
2026-06-11 14:30:55 +0100

    Décrivez les changements apportés

--- Front-End/src/Pages/Users.js
+++ Front-End/src/Pages/Users.js
@@ -227,13 +227,27 @@ function Users() {
                 <div className="user-card-infos">
                   <span
                     className="nn1"
-                    id={us.username === user?.username ? "active-card1" : ""}
+                    id={us.id === user?.id ? "active-card1" : ""}
+                  >
+                    User ID :
+                  </span>
+                  <span
+                    className="nn2"
+                    id={us.id === user?.id ? "active-card1" : ""}
+                  >
+                    {us.id}
+                  </span>
+                </div>
+                <div className="user-card-infos">
+                  <span
+                    className="nn1"
+                    id={us.prenom === user?.prenom ? "active-card1" : ""}
                   >
                     Prenom :
                   </span>
                   <span
                     className="nn2"
-                    id={us.username === user?.username ? "active-card1" : ""}
+                    id={us.prenom === user?.prenom ? "active-card1" : ""}
                   >
                     {us.prenom}
                   </span>
@@ -241,29 +255,15 @@ function Users() {
                 <div className="user-card-infos">
                   <span
                     className="nn1"
-                    id={us.username === user?.username ? "active-card1" : ""}
+                    id={us.nom === user?.nom ? "active-card1" : ""}
                   >
                     Nom :
                   </span>
                   <span
                     className="nn2"
-                    id={us.username === user?.username ? "active-card1" : ""}
+                    id={us.nom === user?.nom ? "active-card1" : ""}
                   >
                     {us.nom}
-                  </span>
-                </div>
-                <div className="user-card-infos">
-                  <span
-                    className="nn1"
-                    id={us.username === user?.username ? "active-card1" : ""}
-                  >
-                    Nom d'utilisateur :
-                  </span>
-                  <span
-                    className="nn2"
-                    id={us.username === user?.username ? "active-card1" : ""}
-                  >
-                    {us.username}
                   </span>
                 </div>
                 <div className="user-card-infos">
